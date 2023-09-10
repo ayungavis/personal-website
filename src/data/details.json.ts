f12f88013b0e5555b7422b89c0d8f57c533e9f1f
Wahyu Kurniawan <ayungavis@gmail.com>
2023-09-11 00:43:00 +0800

    ⚡️ feat: add more work experiences

--- src/data/details.json.ts
+++ src/data/details.json.ts
@@ -12,4 +12,16 @@ export const details: Template[] = [
     location: '🇺🇸 United States',
     date: 'Feb 2022 - Present',
   },
+  {
+    link: 'https://www.xendit.co/',
+    title: 'Software Engineer @ Xendit',
+    location: '🌏 Southeast Asia',
+    date: 'Jul 2021 - Nov 2022',
+  },
+  {
+    link: 'https://sevima.com/',
+    title: 'Full Stack Developer @ Sevima',
+    location: '🇮🇩 Indonesia',
+    date: 'Jun 2019 - May 2021',
+  },
 ];
