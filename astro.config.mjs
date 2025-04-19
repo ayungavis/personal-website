c23e4876368ad7863984f003acd8fef9e57337cc
Wahyu Kurniawan <ayungavis@gmail.com>
2025-04-20 04:06:17 +0700

    chore: migrate to bun and vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://ayungavis.com',
+  site: 'https://lexingtonthemes.com',
   integrations: [tailwind(), sitemap()],
-  output: 'static',
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
 });
